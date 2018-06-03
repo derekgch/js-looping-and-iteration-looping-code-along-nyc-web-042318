1e92a59b62bedc2f3515d8146ab8b00c16e1327d
[Derek] <[derekgc@icloud.com]>
2018-06-03 19:14:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (let i = 0 ;  i < array.length; i ++ ){
+    // debugger;
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+  }
+  return array;
+}
+
+
+
+function tailsNeverFails() {
+  let count = 0;
+  let result = true;
+
+  while (result){
+    if (Math.random() >= 0.5){
+      count ++;
+      result = true;
+    }else {
+      result = false;
+    }
+  }
+  return `You got ${count} tails in a row!`;
+
+}
